1d761797860dc7f4f79e08f61a2a7f88495f434d
dakshtest <dakshjain_ma@srmuniv.edu.in>
2020-12-26 11:37:52 +0530

    g

---
+++ tests/test_api.py
@@ -0,0 +1,22 @@
+import json
+from api import app as flask_app
+import pytest
+
+
+@pytest.fixture
+def app():
+    yield flask_app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+def test_index(app, client):
+    res = client.get('/')
+    assert res.status_code == 200
+    expected = "Hello daksh jain"
+    assert expected == json.loads(res.get_data(as_text=False))
+
+
